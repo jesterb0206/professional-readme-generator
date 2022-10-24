09d8f4bd89aba4d86591fe8b55b7375599596de8
jesterb0206 <jesterb@seattleu.edu>
2022-10-24 01:02:41 -0700

    Render License Badge Function

---
+++ assets/utils/generateMarkdown.js
@@ -0,0 +1,51 @@
+// Returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+
+function renderLicenseBadge(license) {
+  if (selectedLicense === "Apache 2.0 License") {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (selectedLicense === "GNU General Public License v3.0") {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (selectedLicense === "MIT License") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (selectedLicense === "BSD 2-Clause Simplified License") {
+    return `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;
+  } else if (selectedLicense === "BSD 3-Clause New or Revised License") {
+    return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+  } else if (selectedLicense === "Boost Software License 1.0") {
+    return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
+  } else if (selectedLicense === "Creative Commons Zero v1.0 Universal") {
+    return `[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)`;
+  } else if (selectedLicense === "Eclipse Public License 2.0") {
+    return `[![License](https://img.shields.io/badge/License-EPL_2.0-red.svg)](https://opensource.org/licenses/EPL-2.0)`;
+  } else if (selectedLicense === "GNU Affero General Public License v3.0") {
+    return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
+  } else if (selectedLicense === "GNU General Public License v2.0") {
+    return `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
+  } else if (selectedLicense === "GNU Lesser General Public License v3.0") {
+    return `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`;
+  } else if (selectedLicense === "Mozilla Public License 2.0") {
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+  } else if (selectedLicense === "The Unlicense") {
+    return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+  } else {
+    return ``;
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
