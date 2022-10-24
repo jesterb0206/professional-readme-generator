eec55ff560d247eaad78db102d3491d439435f2b
jesterb0206 <jesterb@seattleu.edu>
2022-10-24 01:55:24 -0700

    Render License Section Function

--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -64,15 +64,12 @@ function renderLicenseLink(selectedLicense) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
+// Returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function renderLicenseSection(selectedLicense) {
+  if (selectedLicense == "none") {
+    return ``;
+  } else {
+    return `## License `;
+  }
 }
-
-module.exports = generateMarkdown;
