9b055a7f92a2b9bb22a9b7d637843b344cab9588
jesterb0206 <jesterb@seattleu.edu>
2022-10-24 02:18:19 -0700

    Packages Needed

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,8 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+
+const fs = require("fs");
+const inquirer = require("inquirer");
+const markdown = require("./utilities/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [];
