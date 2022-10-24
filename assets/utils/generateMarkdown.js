2b38dd181bdccf6589947940eaae6bfe44dbce33
jesterb0206 <jesterb@seattleu.edu>
2022-10-24 01:40:47 -0700

    Render License Link Function

--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // Returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 
-function renderLicenseBadge(license) {
+function renderLicenseBadge(selectedLicense) {
   if (selectedLicense === "Apache 2.0 License") {
     return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
   } else if (selectedLicense === "GNU General Public License v3.0") {
@@ -14,8 +14,6 @@ function renderLicenseBadge(license) {
     return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
   } else if (selectedLicense === "Boost Software License 1.0") {
     return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
-  } else if (selectedLicense === "Creative Commons Zero v1.0 Universal") {
-    return `[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)`;
   } else if (selectedLicense === "Eclipse Public License 2.0") {
     return `[![License](https://img.shields.io/badge/License-EPL_2.0-red.svg)](https://opensource.org/licenses/EPL-2.0)`;
   } else if (selectedLicense === "GNU Affero General Public License v3.0") {
@@ -33,9 +31,38 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
+// Returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+
+function renderLicenseLink(selectedLicense) {
+  if (selectedLicense === "Apache 2.0 License") {
+    return `[This application is licensed under Apache](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (selectedLicense === "GNU General Public License v3.0") {
+    return `[This application is licensed under Free Software Foundation](https://opensource.org/licenses/GPL-3.0)`;
+  } else if (selectedLicense === "MIT License") {
+    return `[This application is licensed under MIT](https://opensource.org/licenses/MIT)`;
+  } else if (selectedLicense === "BSD 2-Clause Simplified License") {
+    return `[This application is licensed under BSD](https://opensource.org/licenses/BSD-2-Clause)`;
+  } else if (selectedLicense === "BSD 3-Clause New or Revised License") {
+    return `[This application is licensed under BSD](https://opensource.org/licenses/BSD-3-Clause)`;
+  } else if (selectedLicense === "Boost Software License 1.0") {
+    return `[This application is licensed under Boost](https://opensource.org/licenses/BSL-1.0)`;
+  } else if (selectedLicense === "Eclipse Public License 2.0") {
+    return `[This application is licensed under Eclipse](https://opensource.org/licenses/EPL-2.0)`;
+  } else if (selectedLicense === "GNU Affero General Public License v3.0") {
+    return `[This application is licensed under Free Software Foundation](https://opensource.org/licenses/AGPL-3.0)`;
+  } else if (selectedLicense === "GNU General Public License v2.0") {
+    return `[This application is licensed under Free Software Foundation](https://opensource.org/licenses/GPL-2.0)`;
+  } else if (selectedLicense === "GNU Lesser General Public License v3.0") {
+    return `[This application is licensed under Free Software Foundation](https://opensource.org/licenses/GPL-3.0)`;
+  } else if (selectedLicense === "Mozilla Public License 2.0") {
+    return `[This application is licensed under Mozilla](https://opensource.org/licenses/MPL-2.0)`;
+  } else if (selectedLicense === "The Unlicense") {
+    return `[This application is licensed by Unlicense](https://opensource.org/licenses/unlicense)`;
+  } else {
+    return ``;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
